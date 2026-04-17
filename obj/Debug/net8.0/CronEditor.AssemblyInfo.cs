0a99a7751cd0c96af824db8596cf7f18c090321a
Mohammed Aslam <mdaslamknlsp@zohomail.com>
2026-04-18 01:05:38 +0530

    CronEditor Modal added

--- obj/Debug/net8.0/CronEditor.AssemblyInfo.cs
+++ obj/Debug/net8.0/CronEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CronEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b85330043993f73416fda0b45554f5fdb3c09e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("CronEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CronEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
